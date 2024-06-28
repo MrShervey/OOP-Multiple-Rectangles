62bed8b045a7374cc908fcbd5f27cf333d7fed59
MrShervey <jimshervey2@gmail.com>
2024-06-28 18:10:52 +0100

    1

--- OOP movement/Form1.cs
+++ OOP movement/Form1.cs
@@ -12,19 +12,22 @@ namespace OOP_movement
 {
     public partial class Form1 : Form
     {
+        Player newPlayer = new Player(100, 100, 5, 150, 150, 100);
         //Constructor for the form
         public Form1()
         {
             InitializeComponent();
             this.Size = new Size(800, 600);
+            this.KeyDown += Form1_KeyDown;
+           
             populateForm();
         }
         //Instantiate the base and derived classes
         private void populateForm()
         {
             Random random = new Random();
-            //Instantiating 10 objects from the base classes with random values
-            for (int x = 0; x < 10; x++)
+            //Instantiating 5 objects from the base classes with random values
+            for (int x = 0; x < 5; x++)
             {
                 //Random values for the constructor
                 int posx = random.Next(0, 700);
@@ -33,16 +36,23 @@ namespace OOP_movement
                 int g = random.Next(256);
                 int b = random.Next(256);
                 //Instantiating an object from the base class
-                character newCharacter = new character(posx, posy, r, g, b);
+                Character newCharacter = new Character(posx, posy, r, g, b);
                 this.Controls.Add(newCharacter.sprite);
             }
             //Instantiating the moving derived class
-            movingCharacter moving1 = new movingCharacter(350, 100, 2, 0, 0, 0, "h");
+            MovingCharacter moving1 = new MovingCharacter(350, 100, 2, 0, 0, 0, "h");
             this.Controls.Add(moving1.sprite);
-            movingCharacter moving2 = new movingCharacter(350, 300, 7, 0, 0, 0, "h");
+            MovingCharacter moving2 = new MovingCharacter(350, 300, 7, 0, 0, 0, "h");
             this.Controls.Add(moving2.sprite);
-            movingCharacter moving3 = new movingCharacter(650, 300, 5, 0, 0, 0, "v");
+            MovingCharacter moving3 = new MovingCharacter(650, 300, 5, 0, 0, 0, "v");
             this.Controls.Add(moving3.sprite);
+            //Instantiate the player
+
+            this.Controls.Add(newPlayer.sprite);
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            newPlayer.playerMovement(e.KeyCode);
         }
 
         private void Form1_Load(object sender, EventArgs e){}
--- OOP movement/characters.cs
+++ OOP movement/characters.cs
@@ -5,28 +5,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
 
 namespace OOP_movement
 {
     //Base class
-    internal class character
+    internal class Character
     {
         //seting attributes
         private int startposX, startposy;
         public PictureBox sprite = new PictureBox();
         //constructor
-        public character(int x, int y, int red, int green, int blue) 
+        public Character(int x, int y, int red, int green, int blue) 
         { 
             this.startposX = x;
             this.startposy = y;
             sprite.Left = startposX;
             sprite.Top = startposy;
-            sprite.BackColor = Color.FromArgb(red, green, blue);
+            sprite.BackColor = Color.FromArgb(red, green, blue);                  
         }
     }
 
     //Derived class
-    internal class movingCharacter : character
+    internal class MovingCharacter : Character
     {
         //extra attributes for derived class
         private int speed;
@@ -34,16 +35,16 @@ namespace OOP_movement
         private bool left = true;
         private bool up = true;
         private string direction;
+        Timer myTimer = new Timer();
         //constructor
-        public movingCharacter(int x, int y, int s, int red, int green, int blue, string dir)
+        public MovingCharacter(int x, int y, int s, int red, int green, int blue, string dir)
             :base(x,y,red,green,blue) //constructing the base class
         {
             speed = s;
             direction = dir;
-            Timer myTimer = new Timer();
-            myTimer.Tick += new EventHandler(movement);
             myTimer.Interval = 25;
             myTimer.Start();
+            myTimer.Tick += new EventHandler(movement);
         }
         //Moving the picture box sprite
         public void movement(object sender, EventArgs e)
@@ -89,6 +90,34 @@ namespace OOP_movement
                         up = true;
                     }
                 }
+            }
+        }
+    }
+    internal class Player : Character
+    {
+        private int speed;
+        public Player(int x, int y, int s, int red, int green, int blue)
+            :base(x,y,red,green,blue)
+        {
+            speed = s;
+        }
+        public void playerMovement(Keys key)
+        {
+            if (key == Keys.Left && base.sprite.Left > 5)
+            {
+                base.sprite.Left -= speed;
+            }
+            else if (key == Keys.Right && base.sprite.Left < 730)
+            {
+                base.sprite.Left += speed;
+            }
+            else if (key == Keys.Up && base.sprite.Top > 5)
+            {
+                base.sprite.Top -= speed;
+            }
+            else if (key == Keys.Down && base.sprite.Top < 500)
+            {
+                base.sprite.Top += speed;
             }
         }
     }
