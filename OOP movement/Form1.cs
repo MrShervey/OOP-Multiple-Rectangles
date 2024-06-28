a83415171a908dcb5080128cddf3d4af68eb7bff
j.shervey <j.shervey@YS06-TCR>
2024-06-28 12:39:11 +0100

    Add project files.

---
+++ OOP movement/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_movement
+{
+    public partial class Form1 : Form
+    {
+        Panel panel1 = new Panel();
+        public Form1()
+        {
+            InitializeComponent();
+            panel1.Size = new Size(400, 400);
+            this.Controls.Add(panel1);
+            populateForm();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            panel1.Controls.Clear();
+            populateForm();
+        }
+        private void populateForm()
+        {
+            Random random = new Random();
+            for (int x = 0; x < 10; x++)
+            {
+                int posx = random.Next(100, 400);
+                int posy = random.Next(100, 400);
+                int r = random.Next(256);
+                int g = random.Next(256);
+                int b = random.Next(256);
+                characters newCharacter = new characters(posx, posy, 3, r, g, b);
+                panel1.Controls.Add(newCharacter.sprite);
+            }
+        }
+    }
+}
---
+++ OOP movement/characters.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_movement
+{
+    internal class characters
+    {
+        private int startposX, startposy;
+        private double speed;
+        public PictureBox sprite = new PictureBox();
+        private Random random = new Random();
+        public characters(int x, int y, double s, int red, int green, int blue) 
+        { 
+            this.startposX = x;
+            this.startposy = y;
+            this.speed = s;
+            sprite.Left = startposX;
+            sprite.Top = startposy;
+            sprite.BackColor = Color.FromArgb(red, green, blue);
+        }
+    }
+
+}
