e82967e7eb187adcd2cf6fadbec66ef3ca6dbf0b
MrShervey <49448275+MrShervey@users.noreply.github.com>
2025-06-25 14:04:48 +0100

    Update characters.cs

--- OOP movement/characters.cs
+++ OOP movement/characters.cs
@@ -1,33 +1,71 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.TrackBar;
 
 namespace OOP_movement
 {
     //Base class
-    internal class Character
+    class character
     {
         //seting attributes
         private int startposX, startposy;
         public PictureBox sprite = new PictureBox();
+        protected Timer myTimer = new Timer();
         //constructor
-        public Character(int x, int y, int red, int green, int blue)
-        {
+        public character(int x, int y, int red, int green, int blue) 
+        { 
             this.startposX = x;
             this.startposy = y;
-            sprite.Left = startposX;
-            sprite.Top = startposy;
-            sprite.BackColor = Color.FromArgb(red, green, blue);
+            this.sprite.Left = startposX;
+            this.sprite.Top = startposy;
+            this.sprite.BackColor = Color.FromArgb(red, green, blue);
+            this.sprite.Width = 50;
+            this.sprite.Height = 50;
+            this.myTimer.Interval = 25;
+            this.myTimer.Start();
+        }
+        public virtual void movement()
+        { }
+    }
+    //player derived class
+    class player : character
+    {
+        private int Speed = 0;
+        public player(int x, int y, int s, int red, int green, int blue)
+            : base(x, y, red, green, blue) //constructing the base class
+        {
+            this.sprite.Height = 25;
+            this.sprite.Width = 50;
+            this.Speed = s;
+        }
+
+        public new void movement(Keys key)
+        {
+            if (key == Keys.Left && base.sprite.Left > 5)
+            {
+                base.sprite.Left -= this.Speed;
+            }
+            else if (key == Keys.Right && base.sprite.Left < 730)
+            {
+                base.sprite.Left += this.Speed;
+            }
+            else if (key == Keys.Up && base.sprite.Top > 5)
+            {
+                base.sprite.Top -= this.Speed;
+            }
+            else if (key == Keys.Down && base.sprite.Top < 530)
+            {
+                base.sprite.Top += this.Speed;
+            }
         }
     }
-
-    //Derived class
-    internal class MovingCharacter : Character
+    
+    //Derived class moving box
+    class movingCharacter : character
     {
         //extra attributes for derived class
         private int speed;
@@ -35,20 +73,17 @@ namespace OOP_movement
         private bool left = true;
         private bool up = true;
         private string direction;
-        Timer myTimer = new Timer();
         //constructor
-        public MovingCharacter(int x, int y, int s, int red, int green, int blue, string dir)
+        public movingCharacter(int x, int y, int s, int red, int green, int blue, string dir)
             :base(x,y,red,green,blue) //constructing the base class
         {
-            base.sprite.Size = new Size(150, 50);
-            speed = s;
-            direction = dir;
-            myTimer.Interval = 25;
-            myTimer.Start();
-            myTimer.Tick += new EventHandler(movement);
+            this.speed = s;
+            this.direction = dir;
+            base.myTimer.Tick += new EventHandler(this.movement);
+
         }
         //Moving the picture box sprite
-        public void movement(object sender, EventArgs e)
+        public new void movement(object sender, EventArgs e)
         {
             if(direction == "h")
             {
@@ -91,35 +126,6 @@ namespace OOP_movement
                         up = true;
                     }
                 }
-            }
-        }
-    }
-    internal class Player : Character
-    {
-        private int speed;
-        public Player(int x, int y, int s, int red, int green, int blue)
-            :base(x,y,red,green,blue)
-        {
-            speed = s;
-            base.sprite.Size = new Size(50, 50);
-        }
-        public void movement(Keys key)
-        {
-            if (key == Keys.Left && base.sprite.Left > 5)
-            {
-                base.sprite.Left -= speed;
-            }
-            else if (key == Keys.Right && base.sprite.Left < 680)
-            {
-                base.sprite.Left += speed;
-            }
-            else if (key == Keys.Up && base.sprite.Top > 5)
-            {
-                base.sprite.Top -= speed;
-            }
-            else if (key == Keys.Down && base.sprite.Top < 500)
-            {
-                base.sprite.Top += speed;
             }
         }
     }
